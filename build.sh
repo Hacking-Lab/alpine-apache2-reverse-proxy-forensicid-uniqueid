1ab5529f9c69564ea93004899510f33499ae2522
root <root@hlkali.hacking-lab.com>
2021-01-04 11:49:42 +0100

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:3.2.0 -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:3.2 -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:latest -f Dockerfile .
+
+docker push hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid
+
