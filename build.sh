014ce0257765ecb47128128403580314dcb4aad4
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-06 12:22:26 +0100

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 docker build --no-cache -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:3.2.0 -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:3.2 -t hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:latest -f Dockerfile .
 
 docker push hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid
+docker push hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:3.2
+docker push hackinglab/alpine-apache2-reverse-proxy-forensicid-uniqueid:3.2.0
 
